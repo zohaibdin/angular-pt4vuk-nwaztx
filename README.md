a0ebdbe4917249420dbf724de8dedd9c4f9909ab
zohaibdin <muhammadzohaibshams@gmail.com>
2020-01-06 12:29:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pt4vuk-nwaztx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pt4vuk-nwaztx)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  item;
+  checkoutForm;
+  constructor(private cartService: CartService,private formBuilder: FormBuilder,) { }
+
+  ngOnInit() {
+     this.item = this.cartService.getItems();
+     this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.item = this.cartService.clearItems();
+    this.checkoutForm.reset();
+  }
+}
